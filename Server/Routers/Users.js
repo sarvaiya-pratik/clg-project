4ef5f27fad3542fb5c52d4434e5d9d57a8f14f54
sarvaiya-pratik <pratikrajput125@gmail.com>
2023-07-14 21:27:54 +0530

    a

--- Server/Routers/Users.js
+++ Server/Routers/Users.js
@@ -3,6 +3,9 @@ const { LoginCotrol,SignUpControl } = require("../Controls/User");
 const Router = express.Router();
 
 // CURD create-post update-put read-get detele-delete
+Router.route("/").get((req,res)=>{
+    res.json("Hello");
+})
 Router.route("/signup").post(SignUpControl)
 Router.route("/login").post(LoginCotrol)
 
