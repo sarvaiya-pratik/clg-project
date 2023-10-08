9a5313bf82d6b7c0c52be3b4e0a37cb1c0e3ab50
sarvaiya-pratik <122616520+sarvaiya-pratik@users.noreply.github.com>
2023-10-08 21:22:22 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -20,7 +20,7 @@ import Cart from "./pages/cart/Cart"
 import Spinner from "./pages/login/Spinner"
 import Order from "./pages/cart/address/Order"
 
-axios.defaults.baseURL = "http://localhost:4001"
+axios.defaults.baseURL = "https://clg-project-server-new.vercel.app"
 const App = () => {
   // const [user, setLoginUser] = useState({ _id: 12 })
   const [jdata, setJdata] = useState([])
