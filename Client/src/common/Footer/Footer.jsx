0ec0ad6a09f0f24d287ff0d1543d88f86fbc4b22
Sarvaiya Pratik <pratikrajput125@gmail.com>
2023-10-08 22:28:29 +0530

    aaa

--- Client/src/common/Footer/Footer.jsx
+++ Client/src/common/Footer/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
             <h2 className='heading'>FOLLOW US !</h2>
             <div className="up">
               {/* <a href="https://www.facebook.com/pratik.saravaiya?mibextid=ZbWKwL" target='_blank'> */}
-                <button className="card1" onClick={()=>navigate("/")}>
+                <button className="card1" onClick={()=>navigate("/admin")}>
                   <FaFacebook className='facebook' style={{ color: " #3b5998" }} />
                 </button>
               {/* </a> */}
