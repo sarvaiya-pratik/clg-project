ae2292fd13063db5cea8247c303d23809c44890f
Sarvaiya Pratik <pratikrajput125@gmail.com>
2024-03-14 14:48:14 +0530

    ok

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -133,6 +133,7 @@ const login = async (req, res) => {
         }
 
         const accesToken = generateToken(isExistUser._id)
+        
         return res.cookie('token', accesToken, option)
             .status(200).json({
                 success: true,
